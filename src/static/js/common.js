0b844978e0e30e0ba6470506ad7d277fbd373093
antonovayal97 <121108750+antonovayal97@users.noreply.github.com>
2023-02-27 18:05:33 +0900

    2702

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -138,15 +138,136 @@ navigation: {
 let aboutStageWrap = document.querySelector('.about-stage')
 let aboutWrapper = document.querySelector('.about-stage__swiper-wrapper')
 
-aboutStageWrap.style.height = aboutWrapper.scrollWidth + 'px'
+if(aboutStageWrap)
+{
+  aboutStageWrap.style.height = aboutWrapper.scrollWidth + 'px'
+}
 
-
-console.log(aboutWrapper.offsetWidth)
 // МОДАЛКИ НАЧАЛО
 const myModal = new HystModal({
     linkAttributeName: "data-hystmodal",
     // настройки (не обязательно), см. API
 });
 // МОДАЛКИ КОНЕЦ
-
+let calendar = new Vue({
+        el: '#calendar',
+        data() {
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    return {
+      masks: {
+        weekdays: 'WWWW',
+        title: "MMMM"
+      },
+      attributes: [
+        {
+          key: 1,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-red-600 text-white',
+          },
+          dates: new Date(year, month, 1),
+        },
+        {
+          key: 2,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-blue-500 text-white',
+          },
+          dates: new Date(year, month, 2),
+        },
+        {
+          key: 3,
+          customData: {
+            title: "Министерство труда и социального развития РС(Я)",
+            time: "15:00",
+            class: 'bg-blue-500 text-white',
+          },
+          dates: new Date(year, month, 5),
+        },
+        {
+          key: 4,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-indigo-500 text-white',
+          },
+          dates: new Date(year, month, 5),
+        },
+        {
+          key: 4,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-teal-500 text-white',
+          },
+          dates: new Date(year, month, 7),
+        },
+        {
+          key: 5,
+          customData: {
+            title: "Министерство труда и социального развития РС(Я)",
+            time: "15:00",
+            class: 'bg-pink-500 text-white',
+          },
+          dates: new Date(year, month, 11),
+        },
+        {
+          key: 6,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-orange-500 text-white',
+          },
+          dates: { months: 5, ordinalWeekdays: { 2: 1 } },
+        },
+        {
+          key: 7,
+          customData: {
+            title: "Министерство труда и социального развития РС(Я)",
+            time: "15:00",
+            class: 'bg-pink-500 text-white',
+          },
+          dates: new Date(year, month, 22),
+        },
+        {
+          key: 8,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-red-600 text-white',
+          },
+          dates: new Date(year, month, 25),
+        },
+        {
+          key: 9,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-red-600 text-white',
+          },
+          dates: new Date(year, 2, 1),
+        },
+        {
+          key: 10,
+          customData: {
+            title: 'Министерство труда и социального развития РС(Я)',
+            time: "15:00",
+            class: 'bg-red-600 text-white',
+          },
+          dates: new Date(year, month, 1),
+        },
+      ],
+    };
+  },
+  methods:
+  {
+    changeMonth()
+    {
+      this.$refs.calendar.move(5)
+    }
+  }
+      })
 })
