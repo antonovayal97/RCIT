581d1dd0e4f0284c633ad22fbe5f8bbd80e50551
antonovayal97 <121108750+antonovayal97@users.noreply.github.com>
2023-03-21 17:51:52 +0900

    2103

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded",(event) => {
       // настройки (не обязательно), см. API
   });
 
+  const telPageModal = new HystModal({
+      linkAttributeName: "data-hystmodal",
+      // настройки (не обязательно), см. API
+  });
 
   let header = document.querySelector('.header-main')
   let now = window.scrollY
@@ -282,8 +286,6 @@ document.addEventListener("DOMContentLoaded",(event) => {
 
 
 
-
-
   let calendar = new Vue({
     el: '#calendar',
     data() {
@@ -448,97 +450,6 @@ document.addEventListener("DOMContentLoaded",(event) => {
 })
 
 
-if(document.getElementById('pagination'))
-{
-  let pages = document.getElementById('pagination').dataset.pages;
-  document.getElementById('pagination').innerHTML = createPagination(pages, 1);
-  function createPagination(pages, page) {
-    let gal_items = document.querySelectorAll('.about-orcit__galary-item')
-    gal_items.forEach(item => {
-      item.classList.remove('about-orcit__galary-item__active')
-    });
-    let active_gal = document.getElementById('gal-' + page)
-    active_gal.classList.add('about-orcit__galary-item__active')
-    let str = '<ul>';
-    let active;
-    let pageCutLow = page - 1;
-    let pageCutHigh = page + 1;
-    // Show the Previous button only if you are on a page other than the first
-    if (page > 1) {
-      str += '<li class="page-item previous no"><a onclick="createPagination(pages, '+(page-1)+')" class="paggination__arrow"><svg class="icon icon-uslugi-left-arrow "><use href="./img/svg/sprite.svg#uslugi-left-arrow" xlink:href="./img/svg/sprite.svg#uslugi-left-arrow"></use></svg></a></li>';
-    }
-    else
-      {
-        str += '<li class="page-item previous no"><a class="paggination__arrow"><svg class="icon icon-uslugi-left-arrow "><use href="./img/svg/sprite.svg#uslugi-left-arrow" xlink:href="./img/svg/sprite.svg#uslugi-left-arrow"></use></svg></a></li>';
-      }
-    // Show all the pagination elements if there are less than 6 pages total
-    if (pages < 6) {
-      for (let p = 1; p <= pages; p++) {
-        active = page == p ? "active" : "no";
-        str += '<li class="'+active+'"><a onclick="createPagination(pages, '+p+')">'+ p +'</a></li>';
-      }
-    }
-    // Use "..." to collapse pages outside of a certain range
-    else {
-      // Show the very first page followed by a "..." at the beginning of the
-      // pagination section (after the Previous button)
-      if (page > 2) {
-        str += '<li class="no page-item "><a onclick="createPagination(pages, 1)">1</a></li>';
-        if (page > 3) {
-            str += '<li class="out-of-range"><a onclick="createPagination(pages,'+(page-2)+')">...</a></li>';
-        }
-      }
-      // Determine how many pages to show after the current page index
-      if (page === 1) {
-        pageCutHigh += 2;
-      } else if (page === 2) {
-        pageCutHigh += 1;
-      }
-      // Determine how many pages to show before the current page index
-      if (page === pages) {
-        pageCutLow -= 2;
-      } else if (page === pages-1) {
-        pageCutLow -= 1;
-      }
-      // Output the indexes for pages that fall inside the range of pageCutLow
-      // and pageCutHigh
-      for (let p = pageCutLow; p <= pageCutHigh; p++) {
-        if (p === 0) {
-          p += 1;
-        }
-        if (p > pages) {
-          continue
-        }
-        active = page == p ? "active" : "no";
-        str += '<li class="page-item '+active+'"><a onclick="createPagination(pages, '+p+')">'+ p +'</a></li>';
-      }
-      // Show the very last page preceded by a "..." at the end of the pagination
-      // section (before the Next button)
-      if (page < pages-1) {
-        if (page < pages-2) {
-          str += '<li class="out-of-range"><a onclick="createPagination(pages,'+(page+2)+')">...</a></li>';
-        }
-        str += '<li class="page-item no"><a onclick="createPagination(pages, pages)">'+pages+'</a></li>';
-      }
-    }
-    // Show the Next button only if you are on a page other than the last
-    if (page < pages) {
-      str += '<li class="page-item next no"><a onclick="createPagination(pages, '+(page+1)+')" class="paggination__arrow"><svg class="icon icon-uslugi-right-arrow "><use href="./img/svg/sprite.svg#uslugi-right-arrow" xlink:href="./img/svg/sprite.svg#uslugi-right-arrow"></use></svg></a></li>';
-    }
-    else
-    {
-      str += '<li class="page-item next no"><a class="paggination__arrow"><svg class="icon icon-uslugi-right-arrow "><use href="./img/svg/sprite.svg#uslugi-right-arrow" xlink:href="./img/svg/sprite.svg#uslugi-right-arrow"></use></svg></a></li>';
-    }
-    str += '</ul>';
-    // Return the pagination string to be outputted in the pug templates
-    document.getElementById('pagination').innerHTML = str;
-    return str;
-  }
-
-}
-
-
-
 
 
 
@@ -624,3 +535,103 @@ function closeAllSelect(elmnt) {
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let pages = document.getElementById('pagination').dataset.pages;
+    document.getElementById('pagination').innerHTML = createPagination(pages, 1);
+    function createPagination(pages, page) {
+      let gal_items = document.querySelectorAll('.about-orcit__galary-item')
+      gal_items.forEach(item => {
+        item.classList.remove('about-orcit__galary-item__active')
+      });
+      let active_gal = document.getElementById('gal-' + page)
+      active_gal.classList.add('about-orcit__galary-item__active')
+      let str = '<ul>';
+      let active;
+      let pageCutLow = page - 1;
+      let pageCutHigh = page + 1;
+      // Show the Previous button only if you are on a page other than the first
+      if (page > 1) {
+        str += '<li class="page-item previous no"><a onclick="createPagination(pages, '+(page-1)+')" class="paggination__arrow"><svg class="icon icon-uslugi-left-arrow "><use href="./img/svg/sprite.svg#uslugi-left-arrow" xlink:href="./img/svg/sprite.svg#uslugi-left-arrow"></use></svg></a></li>';
+      }
+      else
+        {
+          str += '<li class="page-item previous no"><a class="paggination__arrow"><svg class="icon icon-uslugi-left-arrow "><use href="./img/svg/sprite.svg#uslugi-left-arrow" xlink:href="./img/svg/sprite.svg#uslugi-left-arrow"></use></svg></a></li>';
+        }
+      // Show all the pagination elements if there are less than 6 pages total
+      if (pages < 6) {
+        for (let p = 1; p <= pages; p++) {
+          active = page == p ? "active" : "no";
+          str += '<li class="'+active+'"><a onclick="createPagination(pages, '+p+')">'+ p +'</a></li>';
+        }
+      }
+      // Use "..." to collapse pages outside of a certain range
+      else {
+        // Show the very first page followed by a "..." at the beginning of the
+        // pagination section (after the Previous button)
+        if (page > 2) {
+          str += '<li class="no page-item "><a onclick="createPagination(pages, 1)">1</a></li>';
+          if (page > 3) {
+              str += '<li class="out-of-range"><a onclick="createPagination(pages,'+(page-2)+')">...</a></li>';
+          }
+        }
+        // Determine how many pages to show after the current page index
+        if (page === 1) {
+          pageCutHigh += 2;
+        } else if (page === 2) {
+          pageCutHigh += 1;
+        }
+        // Determine how many pages to show before the current page index
+        if (page === pages) {
+          pageCutLow -= 2;
+        } else if (page === pages-1) {
+          pageCutLow -= 1;
+        }
+        // Output the indexes for pages that fall inside the range of pageCutLow
+        // and pageCutHigh
+        for (let p = pageCutLow; p <= pageCutHigh; p++) {
+          if (p === 0) {
+            p += 1;
+          }
+          if (p > pages) {
+            continue
+          }
+          active = page == p ? "active" : "no";
+          str += '<li class="page-item '+active+'"><a onclick="createPagination(pages, '+p+')">'+ p +'</a></li>';
+        }
+        // Show the very last page preceded by a "..." at the end of the pagination
+        // section (before the Next button)
+        if (page < pages-1) {
+          if (page < pages-2) {
+            str += '<li class="out-of-range"><a onclick="createPagination(pages,'+(page+2)+')">...</a></li>';
+          }
+          str += '<li class="page-item no"><a onclick="createPagination(pages, pages)">'+pages+'</a></li>';
+        }
+      }
+      // Show the Next button only if you are on a page other than the last
+      if (page < pages) {
+        str += '<li class="page-item next no"><a onclick="createPagination(pages, '+(page+1)+')" class="paggination__arrow"><svg class="icon icon-uslugi-right-arrow "><use href="./img/svg/sprite.svg#uslugi-right-arrow" xlink:href="./img/svg/sprite.svg#uslugi-right-arrow"></use></svg></a></li>';
+      }
+      else
+      {
+        str += '<li class="page-item next no"><a class="paggination__arrow"><svg class="icon icon-uslugi-right-arrow "><use href="./img/svg/sprite.svg#uslugi-right-arrow" xlink:href="./img/svg/sprite.svg#uslugi-right-arrow"></use></svg></a></li>';
+      }
+      str += '</ul>';
+      // Return the pagination string to be outputted in the pug templates
+      document.getElementById('pagination').innerHTML = str;
+      return str;
+    }
